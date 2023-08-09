774f59c163754f1f7ced57d2eae41a97fea8dc05
SAM <shaefulamin04@gmail.com>
2023-08-09 18:47:10 +0700

    feat: styling apereance

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector(".search-form");
+const searchForm = document.querySelector(".search-form");
 const showContainer = document.querySelector(".show-container");
 
 // form.addEventListener("submit", async (e) => {
@@ -23,29 +23,6 @@ const showContainer = document.querySelector(".show-container");
 //   }
 // };
 // axios.get("https://api.tvmaze.com/search/shows?q=ninja")
-axios.get("https://api.tvmaze.com/shows/1/episodes?specials=1").then((res) => {
-  console.log(res);
-  let shows = "";
-  // res.data.forEach((data) => (shows += elementShow(data)));
-  // showContainer.innerHTML = shows;
-  res.data.forEach((data, i) => {
-    if (data?.image?.medium) {
-      console.log(data.image.medium, i);
-    }
-  });
-});
-
-function elementShow(data) {
-  if (data.name !== null && data.image.medium !== null) {
-    console.log("dt", data.image.medium);
-    // return `
-    // <li class="show-wrapper">
-    //       <h2>${data.name}</h2>
-    //       <img src="" alt="" />
-    // </li>
-    // `;
-  }
-}
 
 // form.addEventListener("submit", getTheShows);
 
@@ -80,12 +57,80 @@ function elementShow(data) {
 //   `;
 // }
 
-// await fetch("https://api.tvmaze.com/shows/1/episodes?specials=1")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     let shows = "";
-//     data.forEach((show) => (shows += elementShow(show)));
-//     console.log("show", shows);
-//     showContainer.innerHTML = shows;
+function defaultShows() {
+  fetch("https://api.tvmaze.com/shows/1/episodes?specials=1")
+    .then((res) => res.json())
+    .then((data) => {
+      let shows = "";
+      data.forEach((show) => {
+        if (show?.image?.medium) {
+          shows += elementShow(show);
+        }
+      });
+      return (showContainer.innerHTML = shows);
+    });
+}
+defaultShows();
+
+function elementShow(data) {
+  return `
+    <li class="show-wrapper">
+      <img src="${data.image.medium}"  alt="" class="thumbnail-show"/>
+      <h2 class="title-show">${data.name}</h2>
+      <p class="rating-show">Rating: ${data.rating.average}</p>
+    </li>
+  `;
+}
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const query = searchForm.elements.query.value;
+  searchShows(query);
+  searchForm.elements.query.value = "";
+});
+function searchShows(query) {
+  fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let shows = "";
+      data.forEach((show) => {
+        if (show?.image?.medium) {
+          shows += searchElementShow(show);
+        }
+      });
+      console.log("shw", shows)
+      return (showContainer.innerHTML = shows);
+    });
+}
+function searchElementShow(data) {
+  return `
+    <li class="show-wrapper">
+      <img src="${data.show.image.medium}"  alt="" class="thumbnail-show"/>
+      <h2 class="title-show">${data.show.name}</h2>
+      <p class="rating-show">Rating: ${data.show.rating.average}</p>
+    </li>
+  `;
+}
+// axios.get("https://api.tvmaze.com/shows/1/episodes?specials=1").then((res) => {
+//   console.log(res);
+//   let shows = "";
+//   // res.data.forEach((data) => (shows += elementShow(data)));
+//   // showContainer.innerHTML = shows;
+//   res.data.forEach((data, i) => {
+//     if (data?.image?.medium) {
+//       console.log(data.name, data.image.medium, i);
+//     }
 //   });
-// const res = await fetch("https://api.tvmaze.com/search/shows?q=girls");
+// });
+
+// function elementShow(data) {
+//   if (data.name !== null && data.image.medium !== null) {
+//     console.log("dt", data.image.medium);
+//     // return `
+//     // <li class="show-wrapper">
+//     //       <h2>${data.name}</h2>
+//     //       <img src="" alt="" />
+//     // </li>
+//     // `;
+//   }
+// }
